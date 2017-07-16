67f3a92bd193b89b54eea8ba81802bc86c12028d
faimin <fuxianchao@gmail.com>
2017-07-16 18:14:13 +0800

    update

--- ZDFaceDetectionDemo/Controllers/ZDFaceDetectController.m
+++ ZDFaceDetectionDemo/Controllers/ZDFaceDetectController.m
@@ -5,6 +5,11 @@
 //  Created by Zero.D.Saber on 2017/7/11.
 //  Copyright © 2017年 Zero.D.Saber. All rights reserved.
 //
+// http://developer.apple.com/library/ios/samplecode/SquareCam/SquareCam.zip
+// http://www.jianshu.com/p/61ca3a917fe5
+// https://github.com/daiweiping/Scanner-AVCapture
+// https://github.com/clisuper/CLFaceDetectionImagePicker
+// http://www.jianshu.com/p/b5618066dc2c
 
 #import "ZDFaceDetectController.h"
 #import <AVFoundation/AVFoundation.h>
@@ -15,15 +20,14 @@
 
 @interface ZDFaceDetectController () <AVCaptureVideoDataOutputSampleBufferDelegate>
 {
-    AVCaptureSession *_session;
-    AVCaptureDeviceInput *_captureInput;
-    AVCaptureStillImageOutput *_captureOutput;
-    AVCaptureVideoPreviewLayer *_preview;
-    AVCaptureDevice *_device;
-    
     UIView *m_highlitView[100];
     CGAffineTransform m_transform[100];
 }
+
+@property (nonatomic, strong) AVCaptureSession *session;
+@property (nonatomic, strong) AVCaptureDeviceInput *captureInput;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic, strong) AVCaptureDevice *device;
 
 @property (weak, nonatomic) IBOutlet UIView *cameraView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
@@ -58,9 +62,15 @@
 - (void)initialize {
     //1.创建会话层
     _session = [[AVCaptureSession alloc] init];
-    [_session setSessionPreset:AVCaptureSessionPreset640x480];
     
-    //2.创建、配置输入设备
+    //2.设置session显示分辨率
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        [_session setSessionPreset:AVCaptureSessionPreset640x480];
+    } else {
+        [_session setSessionPreset:AVCaptureSessionPresetPhoto];
+    }
+    
+    //3.创建输入设备
     NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
     for (AVCaptureDevice *device in devices) {
         if (device.position == AVCaptureDevicePositionBack) {
@@ -69,51 +79,62 @@
             if (*error) NSLog(@"%@", *error);
         }
     }
+    if (!_captureInput) return;
+    if ([_session canAddInput:_captureInput]) {
+        [_session addInput:_captureInput];
+    }
     
-    if (!_captureInput)  return;
+    // make a video data output
+    AVCaptureVideoDataOutput *videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
+    // captureOutput.minFrameDuration = CMTimeMake(1, 10);
+    NSDictionary *rgbOutputSettings = @{(__bridge_transfer NSString *)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA)};
+    [videoDataOutput setVideoSettings:rgbOutputSettings];
+    videoDataOutput.alwaysDiscardsLateVideoFrames = YES; // discard if the data output queue is blocked (as we process the still image)
     
-    [_session addInput:_captureInput];
-    
-    ///out put
-    AVCaptureVideoDataOutput *captureOutput = [[AVCaptureVideoDataOutput alloc] init];
-    captureOutput.alwaysDiscardsLateVideoFrames = YES;
-    //captureOutput.minFrameDuration = CMTimeMake(1, 10);
-    
-    dispatch_queue_t queue = dispatch_queue_create("cameraQueue", DISPATCH_QUEUE_SERIAL);
-    [captureOutput setSampleBufferDelegate:self queue:queue];
-    
+    // create a serial dispatch queue used for the sample buffer delegate
+    // a serial dispatch queue must be used to guarantee that video frames will be delivered in order
+    // see the header doc for setSampleBufferDelegate:queue: for more information
+    dispatch_queue_t queue = dispatch_queue_create("VideoDataOutputQueue", DISPATCH_QUEUE_SERIAL);
+    [videoDataOutput setSampleBufferDelegate:self queue:queue];
 #if !__has_feature(objc_arc)
     dispatch_release(queue);
 #endif
-    NSString *key = (__bridge_transfer NSString *)kCVPixelBufferPixelFormatTypeKey;
-    NSNumber *value = @(kCVPixelFormatType_32BGRA);
-    NSDictionary *videoSettings = @{key : value};
-    [captureOutput setVideoSettings:videoSettings];
-    [_session addOutput:captureOutput];
     
-    ///custom Layer
+    if ([_session canAddOutput:videoDataOutput]) {
+        [_session addOutput:videoDataOutput];
+    }
+    
+    // make stillImageoutput
+    AVCaptureStillImageOutput *stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = @{AVVideoCodecKey : AVVideoCodecJPEG};
+    [stillImageOutput setOutputSettings:outputSettings];
+    [_session addOutput:stillImageOutput];
+    
+    // 调整人脸显示方向
+    AVCaptureConnection *videoConnection = [videoDataOutput connectionWithMediaType:AVMediaTypeVideo];
+    videoConnection.enabled = NO;
+    if ([videoConnection isVideoOrientationSupported]) {
+        videoConnection.videoOrientation = AVCaptureVideoOrientationPortrait;
+    }
+    
+    //custom Layer
     [self.view.layer addSublayer:self.customLayer];
     
-    //3.创建、配置输出
-    _captureOutput = [[AVCaptureStillImageOutput alloc] init];
-    NSDictionary *outputSettings = @{AVVideoCodecKey : AVVideoCodecJPEG};
-    [_captureOutput setOutputSettings:outputSettings];
-	[_session addOutput:_captureOutput];
+    //添加一个预览图像的层
+    // AVCaptureVideoPreviewLayer是摄像头的视频预览层
+    _previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    _previewLayer.frame = self.cameraView.bounds;
+    _previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    self.cameraView.layer.masksToBounds = YES;
+    [self.cameraView.layer addSublayer:_previewLayer];
     
-    ////////////
-    _preview = [AVCaptureVideoPreviewLayer layerWithSession: _session];
-    _preview.frame = CGRectMake(0, 0, self.cameraView.frame.size.width, self.cameraView.frame.size.height);
-    _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    
-    [self.cameraView.layer addSublayer:_preview];
+    //启动session,output开始接受samplebuffer回调
     [_session startRunning];
-    
-   // NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
 }
 
 //从摄像头缓冲区获取图像
 #pragma mark -
-#pragma mark AVCaptureSession delegate
+#pragma mark AVCaptureVideoDataOutputSampleBufferDelegate
 - (void)captureOutput:(AVCaptureOutput *)captureOutput
 didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
        fromConnection:(AVCaptureConnection *)connection {
@@ -122,16 +143,22 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 #else
     @autoreleasepool {
 #endif
-        
+        // 为媒体数据设置一个CMSampleBuffer的Core Video图像缓存对象
         CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+        // 锁定pixel buffer的基地址
         CVPixelBufferLockBaseAddress(imageBuffer, 0);
         
+        // 得到pixel buffer的基地址
         uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddress(imageBuffer);
+        // 得到pixel buffer的行字节数
         size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+        // 得到pixel buffer的宽和高
         size_t width = CVPixelBufferGetWidth(imageBuffer);
         size_t height = CVPixelBufferGetHeight(imageBuffer);
         
+        // 创建一个依赖于设备的RGB颜色空间
         CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+        // 用抽样缓存的数据创建一个位图格式的图形上下文（graphics context）对象
         CGContextRef newContext = CGBitmapContextCreate(baseAddress,
                                                         width,
                                                         height,
@@ -139,11 +166,12 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
                                                         bytesPerRow,
                                                         colorSpace,
                                                         kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+        // 根据这个位图context中的像素数据创建一个Quartz image对象
         CGImageRef newImage = CGBitmapContextCreateImage(newContext);
         CGContextRelease(newContext);
         CGColorSpaceRelease(colorSpace);
         
-        UIImage *image= [UIImage imageWithCGImage:newImage scale:1 orientation:UIImageOrientationLeftMirrored];
+        UIImage *image = [UIImage imageWithCGImage:newImage scale:1 orientation:UIImageOrientationLeftMirrored];
         CGImageRelease(newImage);
         image = [image fixOrientation];//图像反转
         
@@ -151,6 +179,7 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
             [self detectForFacesInUIImage:image];
         });
         
+        // 解锁pixel buffer
         CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
         
 #if __has_feature(objc_arc)
@@ -162,15 +191,15 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 
 /// 人脸识别
 - (void)detectForFacesInUIImage:(UIImage *)facePicture {
-    CIImage* image = [CIImage imageWithCGImage:facePicture.CGImage];
+    CIImage *image = [CIImage imageWithCGImage:facePicture.CGImage];
     
-    CIDetector* detector = [CIDetector detectorOfType:CIDetectorTypeFace
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
                                               context:nil
                                               options:@{CIDetectorAccuracy : CIDetectorAccuracyLow}];
     
     NSArray *features = [detector featuresInImage:image];
     //NSLog(@"%f---%f",facePicture.size.width,facePicture.size.height);
-    for(NSUInteger j = 0; m_highlitView[j] != nil; j++) {
+    for (NSUInteger j = 0; m_highlitView[j] != nil; j++) {
         m_highlitView[j].hidden = YES;
     }
 
@@ -191,16 +220,16 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 /// 自画图像
 - (void)addSubViewWithFrame:(CGRect)frame index:(NSUInteger)_index {
     if (m_highlitView[_index] == nil) {
-        m_highlitView[_index]= [[UIView alloc] initWithFrame:frame];
+        m_highlitView[_index] = [[UIView alloc] initWithFrame:frame];
         m_highlitView[_index].layer.borderWidth = 2;
         m_highlitView[_index].layer.borderColor = [[UIColor redColor] CGColor];
         [self.imageView addSubview:m_highlitView[_index]];
         
-        UILabel *label = [[UILabel alloc]init];
+        UILabel *label = [[UILabel alloc] init];
         label.text = @"found face!!!!!!";
         label.backgroundColor = [UIColor clearColor];
         label.textColor = [UIColor whiteColor];
-        label.font = [UIFont systemFontOfSize:20];
+        label.font = [UIFont systemFontOfSize:20.0];
         label.frame = CGRectMake(0, 0, frame.size.width, 20);
         [m_highlitView[_index] addSubview:label];
 #if !__has_feature(objc_arc)
@@ -233,8 +262,7 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 
 /// 摄像头反转
 - (IBAction)turnCamera:(UIButton *)sender {
-    
-    NSArray *inputs = _session.inputs;
+    NSArray *inputs = self.session.inputs;
     for (AVCaptureDeviceInput *input in inputs) {
         AVCaptureDevice *device = input.device;
         if ([device hasMediaType:AVMediaTypeVideo]) {
@@ -277,6 +305,15 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 }
 
 #pragma mark - Property
+
+- (AVCaptureVideoPreviewLayer *)previewLayer {
+    if (!_previewLayer) {
+        _previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+        _previewLayer.frame = self.cameraView.bounds;
+        _previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    }
+    return _previewLayer;
+}
 
 - (CALayer *)customLayer {
     if (!_customLayer) {
